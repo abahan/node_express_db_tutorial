c47d0bfa1675820f95179b35f4074726ad6e9465
Alex Bahan <43921679+abahan@users.noreply.github.com>
2022-01-07 19:34:21 -0500

    delete function, findbyid added

--- index.js
+++ index.js
@@ -1,35 +1,64 @@
-const  express = require('express');
+const express = require('express');
 const lessons = require('./models/dbHelper');
-const  app = express();
+const app = express();
 app.use(express.json());
-// const {Client} = require('pg');
-// require('dotenv').config({path:"process.env"});
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.json({message: "working!"});
+    res.json({ message: "working!" });
 });
 
 app.post('/api/lessons', (req, res) => {
     lessons.add(req.body)
-    .then(lesson => {
-        res.status(200).json(lesson);
-    })
-    .catch(err => {
-        res.status(500).json({message: "cannot add lesson "});
-    });
+        .then(lesson => {
+            res.status(200).json(lesson);
+        })
+        .catch(err => {
+            res.status(500).json({ message: "cannot add lesson " });
+        });
 });
 
-app.get('/api/lessons', (req, res) => { 
+app.get('/api/lessons', (req, res) => {
     lessons.find()
-    .then(lessons => {
-        res.status(200).json(lessons);
-    })
-    .catch(err => {
-        res.status(500).json({message: "unable to retreave lessons"});
+        .then(lessons => {
+            res.status(200).json(lessons);
+        })
+        .catch(err => {
+            res.status(500).json({ message: "unable to retreave lessons" });
+        });
+});
+
+app.get('/api/lessons/:id', (req, res) => {
+    lessons.findById(req.params.id)
+        .then(lesson => {
+            if (lesson) {
+                res.status(200).json(lesson);
+            } else {
+                res.status(404).json({ message: "lesson not found" });  // 404 is not found
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Unable to perform operation!" });
+        });
+});
+
+app.delete('/api/lessons/:id', (req, res) => {
+    const {id} = req.params;
+    lessons.remove(id).then(count => { 
+        if (count > 0) {
+            res.status(200).json({ message: "lesson successfully deleted." });
+        } else {
+            res.status(404).json({ message: "Unable to locate record." });
+        }
+       
+    }).catch(err => {   
+        res.status(500).json({ message: "unable to remove lesson" });
     });
 });
 
 
-app.listen(PORT, () => {"Sever is running on port " + PORT});
+
+
+
+app.listen(PORT, () => { "Sever is running on port " + PORT });
--- models/dbHelper.js
+++ models/dbHelper.js
@@ -4,17 +4,33 @@ const config = require('../knexfile'); // this is the knexfile.js
 const db = knex(config.development); // this is the development part in knexfile.js
 
 module.exports = {
-    add, 
-    find
+  add,
+  find,
+  findById,
+  remove
 };
 // add, find, findById, update, remove
 
 async function add(lesson) {
   const id = await db('lessons').insert(lesson).returning('id');
-  return id[0];/// id alone will be an array
+  return findById(id[0]);/// id alone will be an array
 }
 
- function find(lesson) {
-  
+function find(lesson) {
+
   return db('lessons');
+}
+
+function findById(id) {
+
+  return db('lessons')
+    .where({ id: id })
+    .first();
+}
+
+function remove(id) {
+
+  return db('lessons')
+    .where({ id: id })
+    .del();
 }
