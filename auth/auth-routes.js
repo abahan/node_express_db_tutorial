0d0f433082a6fff1eb42135cf8e4062446a6a0c8
Alex Bahan <43921679+abahan@users.noreply.github.com>
2022-01-11 21:45:00 -0500

    no hashing

--- auth/auth-routes.js
+++ auth/auth-routes.js
@@ -14,8 +14,8 @@ router.post('/register', (req, res) => {
     if (!(username && password)) {
         return res.status(400).json({ message: "Please provide username and password" });
     }
-    const hash = bcjs.hashSync(credentials.password, 10);
-    credentials.password = hash;
+    //const hash = bcjs.hashSync(credentials.password, 10);
+   // credentials.password = hash;
     users.addUser(credentials)
         .then(user => {
             res.status(200).json(user);
